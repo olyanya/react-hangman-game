892c35685079dba7fc05271d1d7805d59d5a6bf4
olyanya <you@example.com>
2024-05-15 15:09:50 -0400

    feat: added HangmanDrawing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -8,7 +11,23 @@ function App() {
   const [guessdLetters, setGuessedLetters] = useState<string[]>([]);
 
   console.log(wordToGuess);
-  return <div className=""></div>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
