67807b70c572e6a5f9b48c88cab2322f93f22811
olyanya <you@example.com>
2024-05-15 14:21:33 -0400

    feat: added initial state setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessdLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
   return <div className=""></div>;
 }
 
